7c5df5509ff104774a69b1bf7fa0fbf83c0c8eea
kakalot1519 <54546592+kakalot1519@users.noreply.github.com>
2020-08-18 21:33:20 +0700

    Add Entities

--- eShopSolution/README.md
+++ eShopSolution/README.md
@@ -1,1 +1,3 @@
 #ASP.NET Core 3.1 project from
+##Install packages
+-Microsoft.EntityFrameworkCore.Tools
---
+++ eShopSolution/eShopSolution.Data/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace eShopSolution.Data.Entities
+{
+    public class Product
+    {
+        public int id { get; set; }
+        public int Price { get; set; }
+        public decimal OriginalPrice { get; set; }
+        public int Stock { get; set; }
+        public int ViewCount { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string SeoAlias { get; set; }
+    }
+}
